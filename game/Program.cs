f54deb05c7706b3df6567b105e2187b569f39322
Tiger <tiger@tiger.blue>
2026-01-11 13:03:18 +0100

    fix: loading actual godot project

--- game/Program.cs
+++ game/Program.cs
@@ -1,11 +1,12 @@
 using Engine = twodog.Engine;
 
-using var engine = new Engine("game", ".");
+using var engine = new Engine("game", "project");
 using var godotInstance = engine.Start();
 
 // Run until quit or finished
 var finished = godotInstance.Iteration();
-while (!finished && !Console.KeyAvailable)
+Console.WriteLine("Godot is running, close window or press 'Q' to quit.");
+while (!finished && (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Q))
 {
     finished = godotInstance.Iteration();
 }
