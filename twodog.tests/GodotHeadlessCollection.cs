22d01529495c1e17da5dad9bb155501db33cd9cb
Tiger Jove <tiger@tiger.blue>
2026-01-13 22:35:53 +0100

    feat: github workflow to build vitepress

---
+++ twodog.tests/GodotHeadlessCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace twodog.tests;
+
+[CollectionDefinition("GodotHeadless", DisableParallelization = true)]
+public class GodotHeadlessCollection : ICollectionFixture<GodotHeadlessFixture>
+{
+    // This class is never instantiated
+}
