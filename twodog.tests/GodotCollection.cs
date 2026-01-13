d8f30d77ad63e2aef25a5093131d252b684aba11
Tiger Jove <tiger@tiger.blue>
2026-01-13 14:31:58 +0100

    feat: test project

---
+++ twodog.tests/GodotCollection.cs
@@ -0,0 +1,7 @@
+namespace twodog.tests;
+
+[CollectionDefinition("Godot", DisableParallelization = true)]
+public class GodotCollection : ICollectionFixture<GodotFixture>
+{
+    // This class is never instantiated
+}
