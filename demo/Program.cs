d30f31b10add765da5f5b6017726f03346c0a2e6
Tiger <tiger@tiger.blue>
2026-01-31 00:05:14 +0100

    feat: demo now 'does' a little something

--- demo/Program.cs
+++ demo/Program.cs
@@ -10,7 +10,9 @@ using var engine = new Engine("demo", projectPath);
 using var godotInstance = engine.Start();
 GD.Print("Hello from GodotSharp.");
 GD.Print("Scene Root: ", engine.Tree.CurrentScene.Name);
-GD.Print("Ticker: ", engine.Tree.CurrentScene.GetNode<Ticker>("Ticker"));
+
+var ticker = engine.Tree.CurrentScene.GetNode<Ticker>("Ticker");
+GD.Print("Ticker: ", ticker);
 
 // You can access the SceneTree via engine.Tree
 
@@ -18,8 +20,11 @@ GD.Print("Ticker: ", engine.Tree.CurrentScene.GetNode<Ticker>("Ticker"));
 Console.WriteLine("Godot is running, close window or press 'Q' to quit.");
 
 while (!godotInstance.Iteration())
+{
     if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
         break;
-godotInstance.Iteration();
-godotInstance.Iteration();
+}
+
+Console.WriteLine($"Engine exited after {ticker.localAccumulator} ticks. (iterations / _process calls)");
+
 Console.WriteLine("Godot is shutting down. Thank you for using 2dog. 🦴");
